5641b760f69369d7129b94e7f10f026eae228d71
adithak <adithak59@gmail.com>
2019-06-12 09:10:44 +0530

    Create dd.py

---
+++ dd.py
@@ -0,0 +1,11 @@
+from itertools import permutations
+q1=list(input())
+w1 = permutations(ls1)
+f=[]
+for i in list(w1):
+    d=''
+    for j in i:
+       a+=j
+    if d not in b:
+       f.append(s)
+       print(d)
