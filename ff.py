2efa86fa157f62b0c5b2496c490abdf2150c1c36
adithak <adithak59@gmail.com>
2019-06-13 11:53:23 +0530

    Create ff.py

---
+++ ff.py
@@ -0,0 +1,5 @@
+a=["I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX"]
+rom=input()
+for i in range(len(a)):
+    if rom==a[i]:
+        print(i+1)
