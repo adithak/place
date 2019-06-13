53a26d4def2930af18d7282284f787435f4b4f69
adithak <adithak59@gmail.com>
2019-06-13 11:58:35 +0530

    Update ff.py

--- ff.py
+++ ff.py
@@ -1,5 +1,5 @@
-a=["I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX"]
+b=["I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX"]
 rom=input()
-for i in range(len(a)):
-    if rom==a[i]:
-        print(i+1)
+for q in range(len(b)):
+    if rom==b[q]:
+        print(q+1)
